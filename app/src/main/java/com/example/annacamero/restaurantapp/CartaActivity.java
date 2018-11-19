43c484ab868f708128c07f6026ed259b6f1a61fa
anna.camero <anna.camero@UPC-CT.UPC.EDU>
2018-11-19 15:22:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/annacamero/restaurantapp/CartaActivity.java
@@ -0,0 +1,13 @@
+package com.example.annacamero.restaurantapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carta);
+    }
+}
