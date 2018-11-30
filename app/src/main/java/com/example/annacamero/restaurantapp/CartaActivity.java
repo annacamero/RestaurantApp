ba7ee7d63029237481c4d09a457c625475cdd208
joan.fluvia.marin <joan.fluvia.marin@gmail.com>
2018-11-30 17:02:35 +0100

    modificat

--- app/src/main/java/com/example/annacamero/restaurantapp/CartaActivity.java
+++ app/src/main/java/com/example/annacamero/restaurantapp/CartaActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +52,17 @@ public class CartaActivity extends AppCompatActivity {
                 test.setText(documentSnapshot.getString("platid"));
             }
         });
+
+        //db.collection("plats").addSnapshotListener(new EventListener<QuerySnapshot>() {
+       //     @Override
+       //     public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+       //         for (DocumentSnapshot doc : documentSnapshots) {
+       //             InfoPlat plat = doc.toObject(InfoPlat.class);
+//
+  //              }
+ //           }
+  //      })
     }
 
     //creem la clase Viewholder
